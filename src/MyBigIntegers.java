bec331104000e1a2cb5938cec84575176c81dff3
jmmoseman <jeremy.moseman@trojans.dsu.edu>
2019-11-05 04:42:18 -0600

    ...

--- src/MyBigIntegers.java
+++ src/MyBigIntegers.java
@@ -2,27 +2,26 @@ import java.util.Arrays;
 
 public class MyBigIntegers {
 
-    static String MBIPlus(String n1, String n2){
+    static String MBIPlus(String n1, String n2) {
         // initialize arrays for length of inputs
-        int[] n1Array = new int[n1.length()];
-        int[] n2Array = new int[n2.length()];
-
-        String result = "";
-
-        // fill array with the number, using substring functions
-        for (int i = n1.length()-1; i >= 0; i--) {
-            n1Array[i] = Integer.parseInt(n1.substring(i, i+1));
-        }
-
-        for (int i = n2.length()-1; i >= 0; i--) {
-            n2Array[i] = Integer.parseInt(n2.substring(i, i+1));
-        }
-
         int plusLen1 = n1.length();
         int plusLen2 = n2.length();
 
+        int[] n1Array = new int[plusLen1];
+        int[] n2Array = new int[plusLen2];
+
+        // fill array with the number, using substring functions
+        for (int i = plusLen1-1; i >= 0; i--) {
+            n1Array[i] = Integer.parseInt(n1.substring(i, i+1));
+        }
+
+        for (int i = plusLen2-1; i >= 0; i--) {
+            n2Array[i] = Integer.parseInt(n2.substring(i, i+1));
+        }
+
         int carry = 0;
         int intResult = 0;
+        StringBuilder sb = new StringBuilder();
 
         // "gradeschool" algorithm
         for (int i = plusLen1-1, k = plusLen2-1; i >= 0 || k >=0; i--) {
@@ -49,16 +48,12 @@ public class MyBigIntegers {
             // because I did this backwards (well it's about the only way to do it??)
             // I used this stringbuilder to add the calculated values together.
 
-            StringBuilder sb = new StringBuilder(result);
-
             sb.insert(0, String.valueOf(intResult));
-
-            result = sb.toString();
 
             k--;
 
         }
 
-        return result;
+        return sb.toString();
     }
 }
